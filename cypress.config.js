7242a787be4b637d744d764627abd65d431caeb5
sabrina-bachene <bachenesabrin2494@gmail.com>
2023-05-10 13:05:05 +0200

    quete4

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,10 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  projectId: 'afyd4v',
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
